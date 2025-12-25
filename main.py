e2667bd2e013f03af157b9dba1437c01f76b5288
jamshidkh2004 <jamshidkh2004@gmail.com>
2025-12-25 06:35:26 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
   """Example Hello World route."""
-  name = os.environ.get("NAME", "World")
+  name = os.environ.get("NAME", "World1")
   return f"Hello {name}!"
+
+@app.route("/student")
+def get_student_name():
+  """Gets a student's name from query params, adds quotes, and returns it."""
+  student_name = request.args.get('name')
+  if student_name:  
+    return f'"djkadfvnk"' 
+  return "No student name provided."
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
