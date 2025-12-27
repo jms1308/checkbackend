a4235e1fc4f3f08164bc90d04877ee6de987654b
jamshidkh2004 <jamshidkh2004@gmail.com>
2025-12-27 08:49:16 +0000

    asas

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,6 @@ from flask import Flask
 from dotenv import load_dotenv
 from app.extensions import db, migrate
 from app.api.expenses import bp as expenses_bp
-from app.api.firestore import firestore_bp
-from app.api.firestore_expenses import firestore_expenses_bp
 
 load_dotenv()
 
@@ -17,7 +15,5 @@ def create_app(config_object='app.settings'):
 
     # Register Blueprints
     app.register_blueprint(expenses_bp, url_prefix='/expenses')
-    app.register_blueprint(firestore_bp, url_prefix='/firestore')
-    app.register_blueprint(firestore_expenses_bp, url_prefix='/firestore-expenses')
 
     return app
